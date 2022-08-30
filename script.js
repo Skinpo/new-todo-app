1a3f43c76552009d26a082f0e7e71e0af453714d
celestine <lestine603@gmail.com>
2022-08-30 09:01:58 +0100

    more styling

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
-let greet = "Good day";
+// const checkIt = document.getElementsByClassName("checkbox");
+// const pass = document.getElementsByClassName("pass");
+// const para = document.getElementsByClassName("para");
 
-console.log(greet)
+// for (var i = 0; i < checkIt.length; i++) {
+// checkIt[i].addEventListener("click", () => {
+//     for (let x = 0; x < pass.length; x++) {
+//         pass[x].style.visibility="visible"; 
+//         }
+// })
+// }
+
+// Follow exactly how u didi it for the Ecommerce site
