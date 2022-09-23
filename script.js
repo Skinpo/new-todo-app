8a0a36c597dcb26014d9df6f516c9146d7047c85
celestine <lestine603@gmail.com>
2022-09-23 15:37:00 +0100

    Adding functionality with javascript

--- script.js
+++ script.js
@@ -1,8 +1,41 @@
 const checkBox = document.querySelector(".checkbox");
 const pass = document.querySelector(".pass");
 const para = document.querySelector(".para");
-// const icon = document.querySelector(".icon-light-mode");
-// const header = document.querySelector(".header-light-mode")
+// const appTitle = document.querySelector(".app-title")
+const icon = document.querySelector(".icon-light-mode");
+const iconDark = document.querySelector(".icon-dark-mode");
+const header = document.querySelector(".header-light-mode")
+
+
+icon.addEventListener("click", () => {
+    // icon.classList.contains("icon-light-mode");
+    // icon.src = "../images/icon-sun.svg"
+    if (icon.classList.contains("icon-light-mode")){
+        icon.classList.replace("icon-light-mode", "icon-dark-mode")
+        iconDark.style.visibility = "visible",
+        
+        header.src = "../images/bg-desktop-dark.jpg"}
+
+    // if (iconDark.classList.contains("icon-dark-mode")){
+    //     icon.src = "../images/icon-moon.svg"
+    // }
+})
+
+iconDark.addEventListener("click", () => {
+    // icon.classList.contains("icon-light-mode");
+    // icon.src = "../images/icon-sun.svg"
+    if (icon.classList.contains("icon-dark-mode")){
+        icon.classList.replace("icon-dark-mode", "icon-light-mode")
+        iconDark.style.visibility = "hidden"
+        
+        header.src = "../images/bg-desktop-light.jpg"}
+
+    // if (iconDark.classList.contains("icon-dark-mode")){
+    //     icon.src = "../images/icon-moon.svg"
+    // }
+})
+
+
 
 checkBox.addEventListener("click", () => {
     pass.style.visibility = "visible";
